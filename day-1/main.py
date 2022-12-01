9683e3676310ce672a26b1ed06f90793ddfa1c60
William <william.hp.frisk@gmail.com>
2022-12-01 15:40:04 +0100

    day-1

---
+++ day-1/main.py
@@ -0,0 +1,51 @@
+
+def find_most_calories(input: list) -> int:
+
+    input = sum_of_parts(input)
+    input = sorted(input)
+
+    return input[-1] + input[-2] + input[-3]
+
+    """
+    max_calorie = 0
+    curr_elf = 0
+    for i in input:
+        if not is_num(i):
+            if curr_elf > max_calorie:
+                max_calorie = curr_elf
+            curr_elf = 0
+        else:
+            curr_elf += int(i)
+        
+    return max_calorie
+    """
+
+def sum_of_parts(arr: list) -> list:
+    output = []
+
+    curr_elf = 0
+    for i in arr:
+        if not is_num(i):
+            output.append(curr_elf)
+            curr_elf = 0
+        else:    
+            curr_elf += int(i)
+
+    return output
+
+
+def is_num(num) -> bool:
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    input_array = []
+    with open('./day-1/puzzle_input.txt') as f:
+        for line in f:
+            input_array.append(line)
+
+    print(find_most_calories(input_array))
